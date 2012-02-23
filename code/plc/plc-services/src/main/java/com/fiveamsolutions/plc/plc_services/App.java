b43430cf5f32d0664ea7ee74a4d1a4a616ce69d7
Abraham Evans-EL <aevansel@5amsolutions.com>
2012-02-23 11:24:23 -0500

    Adding skeleton of maven project.

---
+++ code/plc/plc-services/src/main/java/com/fiveamsolutions/plc/plc_services/App.java
@@ -0,0 +1,13 @@
+package com.fiveamsolutions.plc.plc_services;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ code/plc/plc-web/src/main/java/com/fiveamsolutions/plc/plc_web/App.java
@@ -0,0 +1,13 @@
+package com.fiveamsolutions.plc.plc_web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
